7cc98f18b3d32a1dcc0daf4b2d59bd047386093b
CryptoSnowMickey <topdirector2017@gmail.com>
2022-03-22 17:20:56 +0900

    update

--- Frontend/src/layout/Content.js
+++ Frontend/src/layout/Content.js
@@ -16,21 +16,21 @@ const Content = (props) => {
         <div className='compare-part d-flex justify-content-center mt-4'>
           <div className='p-4 border-right border-bottom' style={{ flex: 1 }}>
             <div className='primary-color'>Your Wallet</div>
-            <div>{props.tokenBalance} (${props.tokenMarketCap}) <img src={COIN1}  className='coin1' width="60" style={{ marginTop: "-7px" }} alt='' />COIN</div>
-            {/* <div>1,000 ($0) <img src={COIN1}  className='coin1' width="60" style={{ marginTop: "-7px" }} alt='' />COIN</div> */}
+            {/* <div>{props.tokenBalance} (${props.tokenMarketCap}) <img src={COIN1}  className='coin1' width="60" style={{ marginTop: "-7px" }} alt='' />COIN</div> */}
+            <div>1,000 ($0) <img src={COIN1}  className='coin1' width="60" style={{ marginTop: "-7px" }} alt='' />COIN</div>
           </div>
           <div className='p-4 border-bottom' style={{ flex: 1 }}>
             <div className='primary-color'>Total Earned</div>
-            <div>(${props.totalEarnedBusd}) <img src={COIN2}  className='coin2' width="40" style={{ marginTop: "-7px", marginRight: "10px" }} alt='' />BUSD</div>
-            {/* <div>($0) <img src={COIN2}  className='coin2' width="40" style={{ marginTop: "-7px", marginRight: "10px" }} alt='' />BUSD</div> */}
+            {/* <div>(${props.totalEarnedBusd}) <img src={COIN2}  className='coin2' width="40" style={{ marginTop: "-7px", marginRight: "10px" }} alt='' />BUSD</div> */}
+            <div>($0) <img src={COIN2}  className='coin2' width="40" style={{ marginTop: "-7px", marginRight: "10px" }} alt='' />BUSD</div>
           </div>
         </div>
       </div>
       <div className='mt-4 d-flex flex-column claim-part'>
         <div className='primary-color'>Rewords Not Claimed</div>
         <div className='mt-1'>(${props.rewardBusd}) <img src={COIN2} width="40" className='coin2' style={{ marginTop: "-7px", marginRight: "10px" }} alt='' />BUSD</div>
-        {/* <div className='mt-1'>($0) <img src={COIN2} width="40" className='coin2' style={{ marginTop: "-7px", marginRight: "10px" }} alt='' />BUSD</div> */}
-        <div><button className='claim-button mt-2' onClick={props.handleClaim}>Claim Manually</button></div>
+        <div className='mt-1'>($0) <img src={COIN2} width="40" className='coin2' style={{ marginTop: "-7px", marginRight: "10px" }} alt='' />BUSD</div>
+        {/* <div><button className='claim-button mt-2' onClick={props.handleClaim}>Claim Manually</button></div> */}
       </div>
     </div>
   );
